b7b4bb9ffd8db1893c869206a549902d1e0cb913
K <kas.bihari@gmail.com>
2025-06-17 09:36:44 +0200

    favicon geupload

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ if (!isset($page_title)) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <link rel="shortcut icon" href="assets/kazora.png" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <script defer src="js/main.js"></script>
 </head>
