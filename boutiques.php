4b407c4770e9d2e2181afe0c62f9bd8845fbd1d6
K <kas.bihari@gmail.com>
2025-06-17 11:33:50 +0200

    boutique bug gefixed (alleen zwarte balk eronder i s verkeerd)

--- boutiques.php
+++ boutiques.php
@@ -1,30 +1,9 @@
 <?php
-session_start();
-
+require 'header.php'; 
 if (!isset($page_title)) {
     $page_title = "Kazora | Boutiques";
 }
 ?>
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($page_title) ?></title>
-
-    <!-- CSS links -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-    <link rel="shortcut icon" href="assets/kazora.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
-    <script defer src="js/main.js"></script>
-</head>
-<body class="bg-black text-light" style="font-family: 'Georgia', serif;">
-
-<?php include 'header.php'; ?>
 
 <!-- Boutique info -->
 <section class="section text-center mt-5">
@@ -40,7 +19,4 @@ if (!isset($page_title)) {
   </div>
 </section>
 
-<?php include 'footer.php'; ?>
-
-</body>
-</html>
+<?php require 'footer.php'; ?>
